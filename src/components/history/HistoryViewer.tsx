11f9ddc0ee1592e0fb251a05156715c0664861de
MaxChang3 <36927158+maxchang3@users.noreply.github.com>
2026-02-12 19:58:20 +0800

    fix: correct SVG attribute casing for clipRule

--- src/components/history/HistoryViewer.tsx
+++ src/components/history/HistoryViewer.tsx
@@ -139,7 +139,7 @@ export const HistoryViewer: FC<HistoryViewerProps> = ({ isOpen, onClose }) => {
                                     <path
                                         fillRule="evenodd"
                                         d="M10.218 11.632a5.5 5.5 0 1 1 1.414-1.414l2.075 2.075a1 1 0 0 1-1.414 1.414l-2.075-2.075ZM10.6 7.1a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-                                        clip-rule="evenodd"
+                                        clipRule="evenodd"
                                     ></path>
                                 </svg>
                             </button>
